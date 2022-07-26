d9fd2ba65e22a0a29d2b685a9d0960510bf6d4f6
Jacob Bundgaard <jacob@jacobbundgaard.dk>
2022-07-26 12:53:31 +0200

    Switch to new input system

--- friendSHIPdestroyer/Assets/Scripts/ThrusterScript.cs
+++ friendSHIPdestroyer/Assets/Scripts/ThrusterScript.cs
@@ -1,24 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ThrusterScript : MonoBehaviour
 {
-    public string RotationInputAxisName = "Horizontal";
+    public bool IsFirst;
+    public string ThrustInputAxisName = "Thrust1";
+    public InputActionAsset InputActionAsset;
     public float RotationForce = 1;
     private Rigidbody _rigidbody;
+    private @Input _input;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _input = new @Input();
+        _input.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var rotationForce = Input.GetAxis(RotationInputAxisName) * RotationForce;
+        var rotationInput = IsFirst ? _input.Thruster1.Rotation : _input.Thruster2.Rotation;
+        var rotationForce = rotationInput.ReadValue<float>() * RotationForce;
         _rigidbody.AddForce(transform.right * rotationForce);
-
+        
     }
 }
